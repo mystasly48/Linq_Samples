0d7e2e45db5d693cd3126af6b30f5fb5abd4d036
Maisu <mystasly48@gmail.com>
2016-10-21 22:17:30 +0900

    Added: Skip

---
+++ Skip.cs
@@ -0,0 +1,9 @@
+using System.Linq;
+public class Skip {
+  public static void Main() {
+    int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    int[] skip = array.Skip(5);
+    // { 6, 7, 8, 9, 10 }
+  }
+}
