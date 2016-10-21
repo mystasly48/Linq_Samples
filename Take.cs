a3004ef556eb34b4b1bb7ee77669173a198762a5
Maisu <mystasly48@gmail.com>
2016-10-21 22:17:39 +0900

    Added: Take

---
+++ Take.cs
@@ -0,0 +1,9 @@
+using System.Linq;
+public class Take {
+  public static void Main() {
+    int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    int[] take = array.Take(5);
+    // { 1, 2, 3, 4, 5 }
+  }
+}
