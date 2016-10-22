1ffdc7dbf8d24c9eeb42a2512174c80a32d505b6
Maisu <mystasly48@gmail.com>
2016-10-22 11:52:46 +0900

    Added: Average

---
+++ Average.cs
@@ -0,0 +1,9 @@
+using System.Linq;
+public class Average {
+  public static void Main() {
+    int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    double average = array.Average();
+    // 5.5
+  }
+}
