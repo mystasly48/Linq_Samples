78434832112cf8fa97cba92ee2ec5a019811ef3d
Maisu <mystasly48@gmail.com>
2016-10-22 12:07:21 +0900

    Updated: Take

--- Take.cs
+++ Take.cs
@@ -3,7 +3,7 @@ public class Take {
   public static void Main() {
     int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    int[] take = array.Take(5);
+    int[] take = array.Take(5).ToArray();
     // { 1, 2, 3, 4, 5 }
   }
 }
