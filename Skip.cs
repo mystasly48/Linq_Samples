faa73152ccd365324419d6587d4cfd602142eb5c
Maisu <mystasly48@gmail.com>
2016-10-22 12:07:12 +0900

    Updated: Skip

--- Skip.cs
+++ Skip.cs
@@ -3,7 +3,7 @@ public class Skip {
   public static void Main() {
     int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    int[] skip = array.Skip(5);
+    int[] skip = array.Skip(5).ToArray();
     // { 6, 7, 8, 9, 10 }
   }
 }
