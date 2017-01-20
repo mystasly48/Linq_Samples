c88ec152548291e06b3fb4a3ee17e3bec50de7d1
Maisu <mystasly48@gmail.com>
2017-01-20 16:32:40 +0900

    Fix Range

--- Range.cs
+++ Range.cs
@@ -1,7 +1,10 @@
 using System.Linq;
 public class Range {
   public static void Main() {
-    int[] range = Enumerable.Range(1, 10).ToArray();
+    int[] range1 = Enumerable.Range(1, 10).ToArray();
     // { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+
+    int[] range2 = Enumerable.Range(5, 3).ToArray();
+    // { 5, 6, 7 }
   }
 }
